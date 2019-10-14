bb2639e3dd25b0577309dc75ffc1d76eb215285c
liupb <lpb.waln@outlook.com>
2019-10-14 14:33:40 +0800

    将swagger数据导出为Excel

--- interfaceTest-demo/comm/apidatas.py
+++ interfaceTest-demo/comm/apidatas.py
@@ -14,35 +14,80 @@ import requests
 import unittest
 import pandas
 from comm.readjson import read
+from openpyxl import Workbook
 
-cookie = read('cookies.json')
-path = os.path.dirname(os.path.dirname(__file__))
-apifile = os.path.join(path, "report\\"+"mobile.xlsx")
+path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'doc')
+url = 'http://172.16.101.224:9200'
 
-exceltile = ['接口集合名称','接口地址','接口请求类型','接口描述']
+total_message = []
+def get_apiurl():
+    '获取API接口数据'
+    r = requests.get(url=url+'/swagger-resources')
+    #print(r.json())
+    names = {}
+    for datas in r.json():
+            name = datas['name']
+            location = datas['location']
+            name = name.split('-')
+            name = name[0]
+            if name == 'tebonx':
+                continue
+            message = [name, location]
+            print(message)
+            apidatas = get_api_doc(location)
+            if apidatas != False:
+                createsheet(apidatas, name)
+                names[name] = len(apidatas)
+            else:
+                continue
+    print('本地落地的接口文件：',names)
+    totalfile = os.path.join(path, 'total.txt')
+    nowtime = time.strftime("%Y-%m-%d %H:%M:%S")
+    with open(totalfile, 'a+') as f:
+        f.write('当前统计时间：%s'%nowtime+'\n')
+        f.write(str(names)+'\n')
 
-collection = 'http://172.16.100.22:6002/v2/api-docs'    #集合+abs接口地址
-notice = 'http://172.16.100.22:9016/v2/api-docs'        #消息推送接口地址
+def get_api_doc(apidoc):
+    '获取的API接口数据'
+    r = requests.get(url=url+apidoc)
+    if r.status_code == 200:
+        info = []
+        respinfo = r.json()
+        info = [respinfo['info']['description'], respinfo['info']['title'], respinfo['host'], respinfo['basePath']]
+        resp = r.json()['paths']
+        #print(paths)
+        api_message = []
+        api_message.append(info)
+        for key in resp.keys():
+                apipath = key   #接口地址
+                print(apipath)
+                apimethods = list(resp[key].keys())  #接口请求类型
+                summarylist = []
+                for apimethod in apimethods:
+                    # print(apimethod)
+                    summary=resp[key][apimethod]['summary'] #接口描述
+                    tags = resp[key][apimethod]['tags'][0]  # 接口集合
+                    # print(tags)
+                    if summary not in summarylist:
+                        summarylist.append(summary)
+                    else:
+                        break
+                    # print(summary)
+                message = [tags, apipath, apimethod, summary]
+                print(message)
+                api_message.append(message)
+        return api_message
+    else:
+        return False
 
-def get_api_doc():
-    'api文档数据转换'
-    r = requests.get(url=collection, cookies=cookie)
-    resp = r.json()['paths']
-    all_message = []
-    for key in resp.keys():
-        
-        apipath = key   #接口地址
-        print(apipath)
-        #print(list(resp[key].keys())[0])
-        apimethod = list(resp[key].keys())[0]  #接口请求类型
-        print(apimethod)
-        summary=resp[key][apimethod]['summary'] #接口描述
-        print(summary)
-        tags = resp[key][apimethod]['tags'][0]
-        message = [tags, apipath, apimethod, summary]
-        all_message.append(message)
-        df = pandas.DataFrame(all_message)
-        df.to_excel(apifile)    #写入Excel
+def createsheet(wtmessage, sheetname):
+    '创建Excel并写入数据'
+    filepath = os.path.join(path, sheetname+'.xlsx')
+    df = pandas.DataFrame(wtmessage)
+    with pandas.ExcelWriter(filepath) as f:
+        df.to_excel(f, sheet_name=sheetname)
 
 if __name__ == '__main__':
-    get_api_doc()
+    # get_api_doc()
+    get_apiurl()
+    # createsheet()
